c561f8156543ba8f1ee8a30111685f3740bed2a3
Елена Малолепская <03054177@mail.ru>
2022-03-20 23:36:21 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int TicketPrice = 205;
+        int OneBonusMile = 20;
+
+        int bonus = TicketPrice / OneBonusMile;
+        System.out.println("Ваш бонус составит: " + bonus);
+    }
+}
